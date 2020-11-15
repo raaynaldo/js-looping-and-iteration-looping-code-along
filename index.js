c020d5d46a4dfb5bfd5fe924c9977d13ed27af1a
raaynaldo <raaynaldo@hotmail.com>
2020-11-14 19:28:05 -0500

    commit tree 9abc14aeb67c5e8c954853cae4b51520be0d5bf7

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, gift) {
+    const newArr = [];
+    for (let i = 0; i < name.length; i++) {
+        newArr.push(`Thank you, ${name[i]}, for the wonderful ${gift} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(count) {
+  while (count != -1) {
+        console.log(count);
+        count--;
+  }
+}
